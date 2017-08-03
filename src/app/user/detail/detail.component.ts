4ec596f5dfea2a9cbc3fcaf13d2a3bd26eea2420
machine <1724554570@qq.com>
2017-08-03 18:25:03 +0800

    initialize

---
+++ src/app/user/detail/detail.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detail',
+  template: `
+    <p>
+      detail Works!
+    </p>
+  `,
+  styles: []
+})
+export class DetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
