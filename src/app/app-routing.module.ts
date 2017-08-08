266bfe04d7e4ae776104c4edd673fa172b9b823f
machine <1724554570@qq.com>
2017-08-08 19:52:35 +0800

    review build

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,7 +20,8 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes, { useHash: true })
+    //RouterModule.forRoot(routes)
   ],
   exports: [
     RouterModule
